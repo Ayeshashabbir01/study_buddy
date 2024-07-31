0fbf4a05d037aa108b059c8d146683e436703a49
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-07-31 12:16:31 +0500

    html

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     
     'base.apps.BaseConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -57,10 +58,7 @@ ROOT_URLCONF = 'studybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
