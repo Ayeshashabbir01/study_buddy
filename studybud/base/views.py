ec55b32764fcf9a6033bb6407c3cf599025d5054
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-07-31 15:40:50 +0500

    html

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
-
+rooms = [
+    {'id': 1, 'name': 'lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Fronted Developer'},
+]
 
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms': rooms}
+    return render(request,'base/home.html' , context)
 
 def room(request):
-    return render(request,'room.html')
+    return render(request,'base/room.html')
