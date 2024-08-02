59ca5be156bf923ee89b99d7dc9cba3682787a49
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-08-02 14:59:37 +0500

    Models

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 rooms = [
     {'id': 1, 'name': 'lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developer'},
+    {'id': 2, 'name': 'Design with me.'},
+    {'id': 3, 'name': 'Frontend developer.'},
 ]
 
 def home(request):
