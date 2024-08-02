24dc83f2ef0170745602ac856244184bc7bb6516
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-08-02 21:55:44 +0500

    admin

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -5,7 +5,7 @@ class Room(models.Model):
     # host 
     # topic 
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True,blank=True)
     # participants 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
