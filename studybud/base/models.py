aa550b4e2df257847f2a999295c7d575c1b83a7b
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-08-05 14:18:38 +0500

    migrations

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -10,5 +10,5 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name 
