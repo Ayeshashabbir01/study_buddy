def3e493251ed6de50ef5b39de9443c7f5aefbfe
Ayesha Shabbir <aishashabbirdatasci207@gmail.com>
2024-08-07 14:49:28 +0500

    room_form.html

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -7,23 +7,27 @@ class Topic(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class Room(models.Model):
-    host  = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL,null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
     # participants 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
-        return self.name 
-    
+        return self.name  
+
 
 class Message(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
@@ -31,5 +35,3 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
-
-
